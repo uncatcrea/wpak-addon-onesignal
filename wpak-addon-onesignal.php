e700a72d264fbf7072915a97bd830548213d224e
Mathieu Le Roi <mathleroi@gmail.com>
2020-10-15 22:23:01 +0200

    Remove minSdkVersion for new export types

--- wpak-addon-onesignal.php
+++ wpak-addon-onesignal.php
@@ -36,7 +36,7 @@ if ( !class_exists( 'WpAppKitOneSignal' ) ) {
             add_filter( 'wpak_app_index_content', array( __CLASS__, 'add_onesignal_script_to_index' ), 10, 3 );
             add_filter( 'wpak_pwa_manifest', array( __CLASS__, 'add_onesignal_pwa_manifest_fields' ), 10, 2 );
             add_filter( 'wpak_pwa_service_worker', array( __CLASS__, 'add_onesignal_service_worker' ), 10, 2 );
-            add_filter( 'wpak_config_xml_custom_preferences', array( __CLASS__, 'add_min_sdk_preference'), 10, 2 );
+            add_filter( 'wpak_config_xml_custom_preferences', array( __CLASS__, 'add_min_sdk_preference'), 10, 3 );
 
             //OneSignal's WordPress plugin integration: allow to send Mobile apps notifications with post deeplink:
             add_filter( 'onesignal_send_notification', array( __CLASS__, 'add_launch_route_to_onesignal_payload'), 10, 4 );
@@ -150,9 +150,9 @@ if ( !class_exists( 'WpAppKitOneSignal' ) ) {
             return $service_worker_content;
         }
 
-        public static function add_min_sdk_preference( $preferences, $app_id ) {
+        public static function add_min_sdk_preference( $preferences, $app_id, $export_type ) {
 
-            if ( WpakAddons::addon_activated_for_app( self::slug, $app_id ) ) {
+            if ( WpakAddons::addon_activated_for_app( self::slug, $app_id ) && $export_type === 'phonegap-build' ) {
                 //Force minSdkVersion to 15 to avoid the "uses-sdk:minSdkVersion 14 cannot be smaller than
                 //version 15 declared in library" error when building on PhoneGap build
                 //(see https://github.com/OneSignal/OneSignal-Cordova-SDK/issues/375#issuecomment-392872631 )
